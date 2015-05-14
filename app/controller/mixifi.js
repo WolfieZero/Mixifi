f1716e519d6ea4131e3b9e7e1899320cbfe639fd
Neil Sweeney <neil@wolfiezero.com>
2015-05-14 13:42:19 +0100

    removed demo links

--- app/controller/mixifi.js
+++ app/controller/mixifi.js
@@ -1,7 +1,7 @@
 Mixifi.controller('mixifi', function( $http, $scope, SpotifyAPI, iTunesAPI ) {
 
     $scope.placeholder   = 'Simply drop your playlist in here to get prices on iTunes';
-    $scope.spotifytracks = 'http://open.spotify.com/track/0hAKD3hdy0RUPiPJFz270S http://open.spotify.com/track/1yUUTiVGC0wNLmnKM55KVX http://open.spotify.com/track/0w4q7zTJKN0oF6JgNl44ZC';
+    $scope.spotifytracks = '';
     $scope.spotify       = SpotifyAPI;
     $scope.itunes        = iTunesAPI;
     $scope.tracks        = [];
